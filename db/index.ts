41f02d61352b7027e496ecb70bf25c89c57ec885
coopdloop <cooper.wallace32599@gmail.com>
2023-08-16 19:35:51 -0500

    initialize drizzle

---
+++ db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+
+const client = postgres({ssl:'require'});
+const db: PostgresJsDatabase = drizzle(client);
+
+const main = async () => {
+    console.log('running')
+};
+
+main();
