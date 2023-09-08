52f78cb256923c7438988546be9fbc5555b6a924
coopdloop <cooper.wallace32599@gmail.com>
2023-09-08 04:31:51 -0500

    create sample init users

--- .docker/setup.sql
+++ .docker/setup.sql
@@ -1,6 +1,12 @@
 -- create the databases
 CREATE DATABASE IF NOT EXISTS my_data;
 -- Insert dummy data
+INSERT INTO users (user_id, username, email) 
+VALUES
+(1,'John Doe', 'jdoe@test.com'),
+(2,'Doe John', 'djohn@test.com'),
+(3,'John John', 'jjohn@test.com'),
+(4,'Doe Doe', 'ddoe@test.com');
 
 
 FLUSH PRIVILEGES;
